b1f980be8022358f0ad889dadd32748509c62e44
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-12 20:57:46 +0300

    Create Quiz: All Tied Up.js

---
+++ Intro to JavasScript/Data Types and Variables/Quiz: All Tied Up.js
@@ -0,0 +1,20 @@
+/*Directions:
+
+Directions:
+Build a single string that resembles the following joke.
+
+Why couldn't the shoes go out and play?
+They were all "tied" up!
+Your joke should take the format of a question and answer. The first line should be a question and the second line should be an answer.
+
+Hint: You will need to use special characters to produce the following output.
+
+Your Code: */
+
+/*
+ * Programming Quiz: All Tied Up (2-5)
+ */
+
+var joke = "Why couldn\'t the shoes go out and play?\nThey were all \"tied\" up!"
+/* write your joke here */
+console.log(joke);
