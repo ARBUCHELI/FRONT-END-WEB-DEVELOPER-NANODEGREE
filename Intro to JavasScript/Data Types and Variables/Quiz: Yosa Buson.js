e4991fdc5d3fa1cd7f027dedcbbd216e9335505e
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-12 20:59:07 +0300

    Create Quiz: Yosa Buson.js

---
+++ Intro to JavasScript/Data Types and Variables/Quiz: Yosa Buson.js
@@ -0,0 +1,18 @@
+/*Directions:
+Build a string using concatenation by combining the lines from this famous haiku poem by Yosa Buson.
+
+Blowing from the west
+Fallen leaves gather
+In the east.
+Each string should be printed on its own line.
+
+Hint: You will need to use special characters to produce the following output. For a refresher, feel free to review the previous Escaping Strings topic in this lesson.
+
+Your Code:*/
+
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+var haiku = "Blowing from the west"+"\n"+"Fallen leaves gather"+"\n"+"In the east."/* concatenate the strings here */
+console.log(haiku);
