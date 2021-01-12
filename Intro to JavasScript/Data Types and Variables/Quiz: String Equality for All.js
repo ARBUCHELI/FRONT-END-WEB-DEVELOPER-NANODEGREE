7942d10b8e3e265cec3107835a6be3d268cc9775
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-12 20:55:39 +0300

    Create Quiz: String Equality for All.js

---
+++ Intro to JavasScript/Data Types and Variables/Quiz: String Equality for All.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: String Equality for All (2-4)
+ */
+
+// fix the right side of the expression so it evaluates to true.
+
+var answer = "ALL Strings are CrEaTeD equal" == "ALL Strings are CrEaTeD equal";
+console.log(answer);
