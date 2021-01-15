f240f9fca3c5d4c4f0d331df4f06af62c2ff14e8
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 15:52:22 +0300

    Create Quiz: 99 Bottles of Juice.js

---
+++ Intro to JavasScript/Loops/Quiz: 99 Bottles of Juice.js
@@ -0,0 +1,60 @@
+/*
+Directions:
+Write a loop that prints out the following song. Starting at 99, and ending at 1 bottle.
+
+99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around... 98 bottles of juice on the wall!
+98 bottles of juice on the wall! 98 bottles of juice! Take one down, pass it around... 97 bottles of juice on the wall!
+...
+2 bottles of juice on the wall! 2 bottles of juice! Take one down, pass it around... 1 bottle of juice on the wall!
+1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!
+Some Notes:
+
+Note the pluralization of the word "bottle" when you go from 2 bottles to 1 bottle.
+Your text editor may try to autocorrect your ellipses (...) to the ellipses character (…). Do not use the ellipses character for this quiz; use three consecutive periods instead.
+Your Code:
+*/
+
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `num` with a starting value of `99`
+ * - Your code should include a `while` loop
+ * - Your `while` loop should have a stop condition
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+var num = 99;
+var botellas1 = "bottles";
+var botellas2 = "bottles";
+
+while (num > 0) {
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+    if (num === 2){
+        botellas1 = "bottles";
+        botellas2 = "bottle";
+        
+    }
+    if (num === 1){
+        botellas1 = "bottle";
+        botellas2 = "bottles";
+    }
+    console.log(num+" "+botellas1+" of juice on the wall! "+num+" "+botellas1+" of juice! Take one down, pass it around... "+(num-1)+" "+botellas2+" of juice on the wall!");
+    num--;
+}
+
