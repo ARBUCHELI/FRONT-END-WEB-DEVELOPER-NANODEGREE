0b922995b92e156696873fed58959b59a59ea2e1
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 16:06:47 +0300

    Create Quiz: Fix the error 2.js

---
+++ Intro to JavasScript/Loops/Quiz: Fix the error 2.js
@@ -0,0 +1,47 @@
+/*
+Directions:
+The for loop below has an error. Fix it!
+
+for (var k = 0 k < 200 k++) {
+  console.log(k);
+}
+Your Code:
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `k` with the value of `0`
+ * - Your `for` loop should have a stop condition for the value of `k`
+ * - Your `for` loop should increment `k` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// fix the for loop
+for (var k = 0; k < 200; k++) {
+    console.log(k);
+}
+
+*/
+
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ */
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should use a `for` loop
+ * - Your `for` loop should specify a starting condition `k` with the value of `0`
+ * - Your `for` loop should have a stop condition for the value of `k`
+ * - Your `for` loop should increment `k` each time it executes
+ * - Your code should produce the expected output, as explained above
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// fix the for loop
+for (var k = 0; k < 200; k++) {
+    console.log(k);
+}
