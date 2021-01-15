c3995bdf01715398c1fbb542618b464630efd718
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 15:37:24 +0300

    Update Quiz: Navigating the Food Chain.js

--- Intro to JavasScript/Conditionals/Quiz: Navigating the Food Chain.js
+++ Intro to JavasScript/Conditionals/Quiz: Navigating the Food Chain.js
@@ -21,3 +21,44 @@ If eatsPlants equals true and eatsAnimals equals false, then herbivore should be
 Your Code:
 */
 
+/*
+ * Programming Quiz - Navigating the Food Chain (3-8)
+ *
+ * Use a series of ternary operator to set the category to one of the following:
+ *   - "herbivore" if an animal eats plants
+ *   - "carnivore" if an animal eats animals
+ *   - "omnivore" if an animal eats plants and animals
+ *   - undefined if an animal doesn't eat plants or animals
+ *
+ * Notes
+ *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+ *   - `if` statements aren't allowed ;-)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have the variables `eatsPlants`, `eatsAnimals`
+ * - Your code should include ternary statements. Do not use if....else statement. 
+ * - Your code should produce the expected output
+ * - Your code should not be empty
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = false;
+var eatsAnimals = false;
+
+/*
+ * Test your code agaist the followig possible input/output combinations of (`eatsPlants`, `eatsAnimals`, expected output):
+ * - (true, true, omnivore)
+ * - (false, true, carnivore)
+ * - (true, false, herbivore)
+ * - (false, false, undefined)
+ */
+
+/* your code goes here */
+var category = (eatsPlants  && !eatsAnimals) ? "herbivore" : (eatsAnimals && !eatsPlants) ? "carnivore" : (eatsPlants && eatsAnimals) ? "omnivore" : "undefined";
+
+console.log(category);
+
+
