b808c1c9c621f035f55924c074e7e8e2923979ac
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 15:33:08 +0300

    Update Quiz: Ice Cream.js

--- Intro to JavasScript/Conditionals/Quiz: Ice Cream.js
+++ Intro to JavasScript/Conditionals/Quiz: Ice Cream.js
@@ -1,6 +1,7 @@
 /*
 Directions:
-Ice cream is one of the most versatile desserts on the planet because it can be done up so many different ways. Using logical operators, write a series of complex logical expressions that prints only if the following conditions are true:
+Ice cream is one of the most versatile desserts on the planet because it can be done up so many different ways. Using logical operators, write a series of complex logical 
+expressions that prints only if the following conditions are true:
 
 if flavor is set to vanilla or chocolate and
 if vessel is set to cone or bowl and
@@ -13,7 +14,8 @@ Fill in the blanks with the flavor of the ice cream, vessel, and toppings. For e
 I'd like two scoops of vanilla ice cream in a cone with peanuts.
 TIP: Make sure to test your code with different values. For example,
 
-If flavor equals "chocolate", vessel equals "cone" and toppings equals "sprinkles", then "I'd like two scoops of chocolate ice cream in a cone with sprinkles." should be printed to the console.
+If flavor equals "chocolate", vessel equals "cone" and toppings equals "sprinkles", then "I'd like two scoops of chocolate ice cream in a cone with sprinkles." should be
+printed to the console.
 */
 
 /*
