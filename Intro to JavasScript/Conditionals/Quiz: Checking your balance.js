a11db5e449fcec108e72de22e7b3f1bd929d3829
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 15:31:19 +0300

    Update Quiz: Checking your balance.js

--- Intro to JavasScript/Conditionals/Quiz: Checking your balance.js
+++ Intro to JavasScript/Conditionals/Quiz: Checking your balance.js
@@ -1,5 +1,6 @@
 /*Directions:
-Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with rounded 
+Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with
+rounded 
 corners represent what should be printed to the console.*/
 
 Use the following variables in your solution:
@@ -9,7 +10,8 @@ isActive - if account is active
 checkBalance - if you want to check balance
 Hint: The variable balance could be a value less than, greater than, or equal to 0. The variables isActive and checkBalance are booleans that can be set to true or false.
 
-TIP: To print out the account balance with decimal points (i.e. 325.00), use the .toFixed() method and pass it the number of decimal points you want to use. For example, balance.toFixed(2) returns 325.00.
+TIP: To print out the account balance with decimal points (i.e. 325.00), use the .toFixed() method and pass it the number of decimal points you want to use. For example,
+ balance.toFixed(2) returns 325.00.
 
 TIP: Make sure to test your code with different values. For example,
 
