8d6cc8e0307db3314f30b699e96fad10f6fb2029
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-01-15 19:35:27 +0300

    Create Quiz: Factorials!.js

---
+++ Intro to JavasScript/Loops/Quiz: Factorials!.js
@@ -0,0 +1,27 @@
+/*
+Directions:
+Write a for (note: not a function) loop that prints out the factorial of the number 12:
+
+A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+
+3! = 3 * 2 * 1 = 63!=3∗2∗1=6
+4! = 4 * 3 * 2 * 1 = 244!=4∗3∗2∗1=24
+5! = 5 * 4 * 3 * 2 * 1 = 1205!=5∗4∗3∗2∗1=120
+
+Save your final answer in a variable called solution and print it to the console.
+
+Your Code:
+*/
+
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+
+let solution = 1;
+for (let i=12; i >= 1; i--){
+    solution = solution*i;
+}
+console.log(solution);
+
