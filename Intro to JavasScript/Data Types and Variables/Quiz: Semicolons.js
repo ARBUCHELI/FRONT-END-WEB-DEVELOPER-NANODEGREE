3aafd3fdffd9f4fe844f6a126eeafc07878f26eb
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-16 19:37:57 +0300

    Update Quiz: Semicolons.js

--- Intro to JavasScript/Data Types and Variables/Quiz: Semicolons.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: Semicolons.js
@@ -1,5 +1,6 @@
-/*One thing to take notice of in all the examples you've seen so far is the use of semicolons ; at the end of each line. Semicolons make it clear where one statement ends and
-another begins. This is especially handy when multiple lines of code are written on the same line (which is valid JavaScript, but definitely not recommended!). For instance:
+/*One thing to take notice of in all the examples you've seen so far is the use of semicolons ; at the end of each line. Semicolons make it clear where one statement
+ends and another begins. This is especially handy when multiple lines of code are written on the same line (which is valid JavaScript, but definitely not recommended!).
+For instance:
 
 var totalAfterTax = 53.03 var tip = 8 // this is incorrect!
 Uncaught SyntaxError: Unexpected token var
@@ -7,6 +8,7 @@ Uncaught SyntaxError: Unexpected token var
 vs.
 
 var totalAfterTax = 53.03; var tip = 8; // this is correct!
+
 Not adding semicolons to the end of each line can cause bugs and errors in your programs. JavaScript does have ways to occasionally predict where semicolons should be, but just 
 like how type coercion can result in some unexpected quirky behavior in JavaScript, it's good practice to not depend on it.
 
