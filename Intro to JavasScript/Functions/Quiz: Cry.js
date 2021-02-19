caaf7e6cedcd96deeda203e8289c0b7103c5cce1
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 12:04:42 +0300

    Update Quiz: Cry.js

--- Intro to JavasScript/Functions/Quiz: Cry.js
+++ Intro to JavasScript/Functions/Quiz: Cry.js
@@ -1,5 +1,6 @@
 Directions:
-Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, 
+not the function name:
 
 cry();
 Returns: boohoo!
