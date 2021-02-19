73cfee1f68e0781d47e1b7d415c7940556e867e6
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:40:58 +0300

    Update Quiz: One awesome message.js

--- Intro to JavasScript/Data Types and Variables/Quiz: One awesome message.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: One awesome message.js
@@ -5,8 +5,8 @@ Hi, my name is Julia. I love cats. In my spare time, I like to play video games.
 Hi, my name is James. I love baseball. In my spare time, I like to read.
 Declare and assign values to three variables for each part of the sentence that changes (firstName, interest, and hobby).
 
-Use your variables and string concatenation to create your own awesome message and store it in an awesomeMessage variable. Finally, print your awesome message to the JavaScript 
-console.
+Use your variables and string concatenation to create your own awesome message and store it in an awesomeMessage variable. Finally, print your awesome message
+to the JavaScript console.
 
 Your Code:*/
 
