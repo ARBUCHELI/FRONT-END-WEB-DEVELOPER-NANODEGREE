06006cec45bf6ce1339734f9551d54fd0bccf1b0
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:27:57 +0300

    Update Quiz: First Expression.js

--- Intro to JavasScript/Data Types and Variables/Quiz: First Expression.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: First Expression.js
@@ -1,3 +1,10 @@
+Directions:
+Write an expression that uses at least 3 different arithmetic operators.
+
+The expression should equal 42.
+
+Hint: +, -, *, /, and % are possible arithmetic operators
+
 /*
  * Programming Quiz: First Expression (2-1)
  *
