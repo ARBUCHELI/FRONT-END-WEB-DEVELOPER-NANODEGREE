5a8506c0d09b9fad9b4a79b422938b72fc9e37fe
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 12:05:37 +0300

    Update Quiz: Inline.js

--- Intro to JavasScript/Functions/Quiz: Inline.js
+++ Intro to JavasScript/Functions/Quiz: Inline.js
@@ -1,3 +1,12 @@
+Directions:
+Call the emotions() function so that it prints the output you see below, but instead of passing the laugh() function as an argument, pass an inline function expression
+instead.
+
+emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
+Prints: "I am happy, haha!"
+
+Your Code:
+
 /*
  * Programming Quiz: Inline Functions (5-6)
  */
