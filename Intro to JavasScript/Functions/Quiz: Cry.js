21950a99e84fc220a6534f37df239284e0c8caf6
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 12:02:51 +0300

    Create Quiz: Cry.js

---
+++ Intro to JavasScript/Functions/Quiz: Cry.js
@@ -0,0 +1,27 @@
+Directions:
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!". Don't forget to call the function using the variable name, not the function name:
+
+cry();
+Returns: boohoo!
+
+Your Code:
+
+/*
+ * Programming Quiz: Cry (5-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `cry`
+ * - Your code should include a named function expression stored in the variable `cry`
+ * - Your code should call the function expression using the variable name, not the function name
+ * - Your function expression should return the expected output
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+var cry = function llorar(){
+    return "boohoo!";
+}
+console.log(cry());
