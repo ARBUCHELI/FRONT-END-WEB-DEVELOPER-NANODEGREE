a90a063e74150b8564dad48cdf9154c68d7c5f51
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:29:01 +0300

    Update Quiz: Converting Temperatures.js

--- Intro to JavasScript/Data Types and Variables/Quiz: Converting Temperatures.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: Converting Temperatures.js
@@ -1,3 +1,12 @@
+To convert Celsius to Fahrenheit, you can use the following formula:
+
+F=C\times1.8+32F=C×1.8+32
+
+Directions:
+Use this equation and the variables fahrenheit and celsius to print the Fahrenheit equivalent of 12°C.
+
+NOTE: "12°C" reads as "12 degrees Celsius".
+
 /*
  * Programming Quiz: Converting Tempatures (2-2)
  *
