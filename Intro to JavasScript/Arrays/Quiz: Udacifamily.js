524530c9c566d8fff90a4c1e286c58bdc371d1f5
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 12:07:16 +0300

    Update Quiz: Udacifamily.js

--- Intro to JavasScript/Arrays/Quiz: Udacifamily.js
+++ Intro to JavasScript/Arrays/Quiz: Udacifamily.js
@@ -1,3 +1,8 @@
+Directions:
+Create an array called udaciFamily and add "Julia", "James", and your name to the array. Then, print the udaciFamily to the console using console.log.
+
+Your Code:
+
 /*
  * Programming Quiz: UdaciFamily (6-1)
  */
