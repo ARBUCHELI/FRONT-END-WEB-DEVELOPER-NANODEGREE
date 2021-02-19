acce64a5f5271279d80055df180a54301876f3d4
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:40:11 +0300

    Update Quiz: Mad Libs.js

--- Intro to JavasScript/Data Types and Variables/Quiz: Mad Libs.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: Mad Libs.js
@@ -24,5 +24,6 @@ var adjective2 = 'fun';
 var adjective3 = 'entertaining';
 
 // your code goes here
-var madLib = "The Intro to JavaScript course is "+adjective1+". James and Julia are so "+adjective2+". I cannot wait to work through the rest of this "+adjective3+" content!";
+var madLib = "The Intro to JavaScript course is "+adjective1+". James and Julia are so "+adjective2+". I cannot wait to work through the rest of this "+adjective3+"
+content!";
 console.log(madLib);
