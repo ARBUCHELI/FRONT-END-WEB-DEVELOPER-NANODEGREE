e698eb5e8a0cf02ef2ab8a67b4c1b0dd3bb8682b
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:18:33 +0300

    Create Quiz: Inline.js

---
+++ Intro to JavasScript/Functions/Quiz: Inline.js
@@ -0,0 +1,30 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+ 
+ /*
+ * QUIZ REQUIREMENTS
+ * - Your code should have an `emotions()` function
+ * - Your code should call the `emotions()` function
+ * - Your `emotions()` function call should have an inline function expression passed as the second parameter
+ * - Your function expression should return the expected output
+ */
+
+
+// don't change this code
+// emotions() function definition
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// Call the emotions() function with two arguments
+// Argument 1 - "happy" string
+// Argument 2 - an inline function expression
+emotions("happy", function laugh(n){
+    var risa = "";
+    for (let i=0; i<n; i++){
+        risa+= "ha";
+    }
+    return risa+"!";
+})
