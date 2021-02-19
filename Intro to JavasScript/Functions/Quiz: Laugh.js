9c98e59cd30a8d652d660dd2b6245f5a64fda7a3
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 12:01:51 +0300

    Update Quiz: Laugh.js

--- Intro to JavasScript/Functions/Quiz: Laugh.js
+++ Intro to JavasScript/Functions/Quiz: Laugh.js
@@ -1,3 +1,9 @@
+Directions:
+Write an anonymous function expression that stores a function in a variable called "laugh" and outputs the number of "ha"s that you pass in as an argument.
+
+laugh(3);
+Returns: hahaha!
+
 /*
  * Programming Quiz: Laugh (5-4)
  */
