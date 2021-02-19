a4345fbdea5556d0252ad92c42e242b2ac0d208d
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:31:34 +0300

    Update Quiz: First Expression.js

--- Intro to JavasScript/Data Types and Variables/Quiz: First Expression.js
+++ Intro to JavasScript/Data Types and Variables/Quiz: First Expression.js
@@ -5,6 +5,19 @@ The expression should equal 42.
 
 Hint: +, -, *, /, and % are possible arithmetic operators
 
+Your Code:
+/*
+ * Programming Quiz: First Expression (2-1)
+ *
+ * Write an expression that uses at least three, different, arithmetic operators
+ * to log the number 42 to the console.
+ */
+
+// this expression equals 4, change it to equal 42
+console.log(11 * 4 + 0 - 2);
+
+
+
 /*
  * Programming Quiz: First Expression (2-1)
  *
