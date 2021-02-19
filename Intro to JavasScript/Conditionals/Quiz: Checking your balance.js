cfe90baa0fde78b1ad80cb57418541d481ac9100
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:47:52 +0300

    Update Quiz: Checking your balance.js

--- Intro to JavasScript/Conditionals/Quiz: Checking your balance.js
+++ Intro to JavasScript/Conditionals/Quiz: Checking your balance.js
@@ -1,7 +1,6 @@
 /*Directions:
 Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with
-rounded 
-corners represent what should be printed to the console.*/
+rounded corners represent what should be printed to the console.*/
 
 Use the following variables in your solution:
 
