63d6c85ff8630ea231f86e0e4f55528a4bac8017
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:08:36 +0300

    Create Quiz: Laugh it Off.js

---
+++ Intro to JavasScript/Functions/Quiz: Laugh it Off.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a `laugh()` function
+ * - Your `laugh()` function should return the correct output
+ * - Your code should print `\"hahahahahahahahahaha!\"` by calling the `laugh()` function inside `console.log()`
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+
+
+// your code goes here
+function laugh(){
+    return "hahahahahahahahahaha!";
+}
+console.log(laugh());
