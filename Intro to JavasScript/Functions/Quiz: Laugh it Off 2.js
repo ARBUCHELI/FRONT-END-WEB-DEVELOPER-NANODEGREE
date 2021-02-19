48a464338ae797f8002acfe743012c0776adde30
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-02-19 11:59:31 +0300

    Update Quiz: Laugh it Off 2.js

--- Intro to JavasScript/Functions/Quiz: Laugh it Off 2.js
+++ Intro to JavasScript/Functions/Quiz: Laugh it Off 2.js
@@ -1,3 +1,13 @@
+Directions:
+Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+
+TIP: You might need a loop to solve this!
+
+Here's an example of the output and how to call the function that you will write:
+
+console.log(laugh(3));
+Prints: "hahaha!"
+
 /*
  * Programming Quiz: Laugh it Off 2 (5-2)
  *
