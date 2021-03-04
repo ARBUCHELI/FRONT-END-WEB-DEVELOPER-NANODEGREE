6162d9750dfbce416a952e66b83a924d45ab22bd
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-04 19:59:01 +0300

    Create Quiz: Building the Crew.js

---
+++ Intro to JavasScript/Arrays/Quiz: Building the Crew.js
@@ -0,0 +1,39 @@
+The space western TV drama Firefly premiered in the United States on September 20, 2002. Although the show only featured fourteen episodes and was canceled during its first 
+season, it continues to remain popular today with a growing fan base. In the show, the captain Mal, played by Nathan Fillion, leads a crew of renegades on the spaceship Serenity.
+
+Directions:
+Create an array called crew to organize the Serenity’s crew and set it equal to the variables below . You don't need to type out the actual strings, just use the provided 
+variables.
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+Then, print the crew array to the console.
+
+Your Code:
+
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code should have a variable `crew`
+ * - The variable `crew` should be an array containing the Serenity\'s crew
+ * - Your code should print `crew` to the console as an array. Do not iterate over the elements. 
+ * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
+ */
+ 
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+console.log(crew);
