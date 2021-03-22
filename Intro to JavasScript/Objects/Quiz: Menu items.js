a21d697919c8aee5277a5ec48e65cf589db6218d
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-22 09:43:24 +0300

    Create Quiz: Menu items.js

---
+++ Intro to JavasScript/Objects/Quiz: Menu items.js
@@ -0,0 +1,22 @@
+Directions:
+Create a breakfast object to represent the following menu item:
+
+The Lumberjack - $9.95
+eggs, sausage, toast, hashbrowns, pancakes
+The object should contain properties for the name, price, and ingredients.
+
+Your Code:
+
+/*
+ * Programming Quiz: Menu Items (7-2)
+ * Create an object named `breakfast`. 
+ * The object should contain properties for the `name`, `price`, and `ingredients`.
+ * Print the entire object on the console
+ */
+
+// your code goes here
+var breakfast = {
+    name: "The Lumberjack",
+    price: "$9.95",
+    ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+}
