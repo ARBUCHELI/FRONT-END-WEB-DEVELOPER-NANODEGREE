2dc93953b28a6cd5bda77a6d999d6253656abb71
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-26 10:59:37 +0300

    Create Quiz: Donuts Revisited.js

---
+++ Intro to JavasScript/Objects/Quiz: Donuts Revisited.js
@@ -0,0 +1,41 @@
+Here is an array of donut objects.
+
+var donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+];
+Directions:
+Use the forEach() method to loop over the array and print out the following donut summaries using console.log.
+
+Jelly donuts cost $1.22 each
+Chocolate donuts cost $2.45 each
+Cider donuts cost $1.59 each
+Boston Cream donuts cost $5.99 each
+Your Code:
+
+/*
+ * Programming Quiz: Donuts Revisited (7-6)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * - Your code sshould have an array named `donuts`
+ * - Your `donuts` array should call the `forEach()` method
+ * - Your `forEach()` loop should output the donut summaries
+ * - BE CAREFUL ABOUT THE PUNCTUATION, SPACES, AND EXACT WORDS TO BE PRINTED.
+ */
+
+// This is an array of objects. 
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+// your code goes here
+donuts.forEach(function(object) {
+    console.log(object.type+" donuts cost $"+object.cost+" each");
+});
