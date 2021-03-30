675a037fe5a82ce8045cbe880a3990538f62b30c
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-30 17:27:28 +0300

    Delete Intro to jQuery/The Tricks: DOM Manipulation/Quiz: you.toggleClass().js directory

--- Intro to jQuery/The Tricks: DOM Manipulation/Quiz: you.toggleClass().js/app.js
+++
@@ -1,8 +0,0 @@
-/*
-For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
-*/
-
-// don't change this variable!
-var featuredArticle;
-
-featuredArticle = $(".featured").toggleClass("featured");
