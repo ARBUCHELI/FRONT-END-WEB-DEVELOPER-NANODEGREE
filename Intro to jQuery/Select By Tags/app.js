c75a713eb05fc6ce18579fadeaa02cee85d61343
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-31 18:44:33 +0300

    Delete app.js

--- Intro to jQuery/Select By Tags/app.js
+++
@@ -1,9 +0,0 @@
-/*
-For this quiz, use a jQuery tag selector to grab all of the <li>s on the page!
-*/
-
-// Start with this variable! (don't delete it!)
-var listElements;
-
-listElements = $('li');// your code goes here!
-console.log(listElements);
